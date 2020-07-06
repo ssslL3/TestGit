3129fa3738c7c8e9f1d46ce3c38ece69eaf99b47
LAPTOP-0L5EAPED\lbz <1713806561@qq.com>
2020-07-06 21:13:46 +0800

    test

---
+++ game-of-life-java-master/src/test/java/CellTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import gof.core.Cell;
+
+public class CellTest {
+
+    @Test
+    public void testUpdateState() {
+        Cell c = new Cell();
+
+        c.setNewState(true);
+        c.updateState();
+        assertEquals(true, c.getState());
+    }
+    
+    @Test
+    public void testConstructor() {
+        Cell c = new Cell(true);
+
+        assertEquals(true, c.getState());
+    }
+
+}
